fc39b1e2a347ec18b26301bcb5dcd2fc0e734255
r0bban <robert.p.svensson@gmail.com>
2022-12-07 22:17:27 +0100

    eslit/prettier

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
+    {/*  change basename to repo-name before build for gh-pages*/}
     <BrowserRouter basename={'/'}>
       <App />
     </BrowserRouter>
