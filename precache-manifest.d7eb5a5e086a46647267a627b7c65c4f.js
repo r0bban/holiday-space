f4be0a4607a499ba298b6b08d1556aa70306692e
r0bban <54702466+r0bban@users.noreply.github.com>
2023-12-18 02:59:48 +0100

    Updates

---
+++ precache-manifest.d7eb5a5e086a46647267a627b7c65c4f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "87df31cfabaa8ab39ed1",
+    "url": "/holiday-space/static/css/main.ec86443f.chunk.css"
+  },
+  {
+    "revision": "87df31cfabaa8ab39ed1",
+    "url": "/holiday-space/static/js/main.db6c70c6.chunk.js"
+  },
+  {
+    "revision": "b6edb9142deee659d951",
+    "url": "/holiday-space/static/js/runtime~main.3858e751.js"
+  },
+  {
+    "revision": "20be191ae8235d9623d7",
+    "url": "/holiday-space/static/js/2.d1449237.chunk.js"
+  },
+  {
+    "revision": "e38bf8adf13e8ac61dc2",
+    "url": "/holiday-space/static/js/3.127fa833.chunk.js"
+  },
+  {
+    "revision": "01ffedbc7b32cd49f1951d55ab5aa7ff",
+    "url": "/holiday-space/static/media/ham.01ffedbc.png"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/holiday-space/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "152338b29233c5893a28b62300bb7162",
+    "url": "/holiday-space/index.html"
+  }
+];
